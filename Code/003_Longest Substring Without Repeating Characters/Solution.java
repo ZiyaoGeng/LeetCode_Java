9caee021edcc2141875dd8a4ba3de7fae053f56d
BlackSpaceGZY <593947521@qq.com>
2017-11-27 22:28:31 +0800

    Code file

--- Code/003_Longest Substring Without Repeating Characters/Solution.java
+++ Code/003_Longest Substring Without Repeating Characters/Solution.java
@@ -1,5 +1,10 @@
 import java.util.Arrays;
 
+/*
+ * 有两个指针，left和遍历数组时的i
+ * 不同的字符的索引存放到数组中（索引为ASCII码）
+ * 第二次找到该字符时，改变left到第一个字符的位置
+ */
 public class Solution {
 	int lengthOfLongestSubstring(String s) {  
 		int []m=new int[256];
