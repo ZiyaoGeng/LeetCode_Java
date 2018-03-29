ee9398fc43cb3d9b51f5bec22d52649792ca8307
BlackSpaceGZY <593947521@qq.com>
2018-03-29 13:02:33 +0800

    Code file

---
+++ Code/107_Binary Tree Level Order Traversal II/Solution.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int val){
+        this.left=right;
+    }
+}
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>>list=new ArrayList<>();
+            if(root==null)
+                return list;
+            Queue<TreeNode> queue=new LinkedList<>();
+            queue.add(root);
+            int level=1;
+            while(!queue.isEmpty()){
+                int newLevel=0;
+                List<Integer>list1=new ArrayList<>();
+                for(int i=0;i<level;i++){
+                    TreeNode node=queue.poll();
+                    list1.add(node.val);
+                    if(node.left!=null) {
+                        queue.offer(node.left);
+                        newLevel++;
+                    }
+                    if(node.right!=null) {
+                        queue.offer(node.right);
+                        newLevel++;
+                    }
+                }
+                list.add(0,list1);   //ÏòÇ°Ìí¼Ó
+                level=newLevel;
+            }
+            return list;
+    }
+}
