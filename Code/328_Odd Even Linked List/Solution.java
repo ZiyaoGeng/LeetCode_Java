19e58a29c53fb031d76ac7f527958d0cd27d8f92
BlackSpaceGZY <593947521@qq.com>
2018-04-15 12:31:32 +0800

    Code file

---
+++ Code/328_Odd Even Linked List/Solution.java
@@ -0,0 +1,23 @@
+
+class ListNode{
+    int val;
+    ListNode next;
+    public ListNode(int val){
+        this.val=val;
+    }
+}
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head != null) {
+            ListNode odd = head, even = head.next, evenHead = even;
+            while (even != null && even.next != null) {
+                odd.next = odd.next.next;
+                even.next = even.next.next;
+                odd = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+        }
+        return head;
+    }
+}
