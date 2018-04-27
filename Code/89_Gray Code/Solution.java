89e170c9ed95f3ba16c04950def4f2b30cf77881
BlackSpaceGZY <593947521@qq.com>
2018-04-27 12:55:02 +0800

    Code file

---
+++ Code/89_Gray Code/Solution.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer>list=new ArrayList<>();
+        for (int i = 0; i < 1<<n; i++)
+            list.add(i ^ i>>1);
+        return list;
+    }
+}
