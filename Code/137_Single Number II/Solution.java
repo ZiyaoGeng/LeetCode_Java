ea6b937d494af3067c5667d89afc3b25c31193e1
BlackSpaceGZY <593947521@qq.com>
2018-05-03 13:02:13 +0800

    Code file

---
+++ Code/137_Single Number II/Solution.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(!map.containsKey(nums[i]))
+                map.put(nums[i],1);
+            else if(map.get(nums[i])==1)
+                map.put(nums[i],2);
+            else
+                map.remove(nums[i]);
+        }
+        return map.keySet().iterator().next();
+    }
+}
