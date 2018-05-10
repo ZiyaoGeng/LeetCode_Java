5c90fdc39218fa325f359fdc9a4c8f878d5cb34f
BlackSpaceGZY <593947521@qq.com>
2018-05-10 18:36:24 +0800

    Code file

---
+++ Code/103_Binary Tree Zigzag Level Order Traversal/Solution.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int val){
+        this.val=val;
+    }
+}
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null)
+            return result;
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            int nodes = queue.size();
+            List<Integer> levelNodes = new ArrayList<>();
+            for(int i = 0; i < nodes; i++) {
+                TreeNode current = queue.poll();
+                if(result.size() % 2 == 0) {
+                    levelNodes.add(current.val);
+                }
+                else {
+                    levelNodes.add(0, current.val);
+                }
+                if(current.left != null) {
+                    queue.add(current.left);
+                }
+                if(current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+            result.add(levelNodes);
+        }
+        return result;
+    }
+}
