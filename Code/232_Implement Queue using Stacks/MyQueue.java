524a4659ddb8ac27b957ab06175c225e9b93f845
BlackSpaceGZY <593947521@qq.com>
2018-06-11 09:50:50 +0800

    Code file

---
+++ Code/232_Implement Queue using Stacks/MyQueue.java
@@ -0,0 +1,58 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
+public class MyQueue {
+    ListNode root;
+    ListNode p;
+    int length;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        root=new ListNode(0);
+        p=root;
+        length =0;
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        p.next=new ListNode(x);
+        p=p.next;
+        length++;
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if(root.next==null)
+            return -1;
+        int d=root.next.val;
+        root.next=root.next.next;
+        length--;
+        this.p=root;
+        while (p.next!=null)
+            p=p.next;
+        return d;
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        return root.next.val;
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        if(root.next==null)
+            return true;
+        return false;
+    }
+    public static void main(String[]args){
+        MyQueue q=new MyQueue();
+        q.push(1);
+        q.pop();
+        q.push(2);
+        System.out.println(q.peek());
+    }
+}
