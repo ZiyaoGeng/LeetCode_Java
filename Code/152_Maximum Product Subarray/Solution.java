428340d6c85d63bdafc6fd72cae9c452c808cddc
BlackSpaceGZY <593947521@qq.com>
2018-06-21 12:44:47 +0800

    Code file

---
+++ Code/152_Maximum Product Subarray/Solution.java
@@ -0,0 +1,22 @@
+
+public class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int max = nums[0], min = nums[0], result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int temp = max;
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+            if (max > result) {
+                result = max;
+            }
+        }
+        return result;
+    }
+    public static void main(String[]args){
+        int[]nums={2,3,-2,-4};
+        new Solution().maxProduct(nums);
+    }
+}
